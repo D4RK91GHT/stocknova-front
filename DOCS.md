d52bc5e032fea912b27d74764e5f554de46d48b1
D4RK91GHT <rahulmajumdar400@gmail.com>
2024-04-10 22:35:54 +0530

    git initilized

---
+++ DOCS.md
@@ -0,0 +1,11 @@
+Logo Colours:
+#0B0303
+#2E1C69
+
+Canva Logo Link:
+https://www.canva.com/design/DAGB8MOMOxc/7qWQMRGyca7HudnDKAPKbw/edit?utm_content=DAGB8MOMOxc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton
+
+
+1. Each Should Have Same Menu/Navbar
+2. Required To Remove:
+    Home: TESTIMONIAL 00, RECENT BLOG
---
+++ src/pages/HomeTwo.js
@@ -0,0 +1,57 @@
+import React from "react";
+import AboutAreaTwo from "../components/AboutAreaTwo";
+import BannerTwo from "../components/BannerTwo";
+import BlogAreaTwo from "../components/BlogAreaTwo";
+import ContactAreaTwo from "../components/ContactAreaTwo";
+import CounterAreaTwo from "../components/CounterAreaTwo";
+import NavbarTwo from "../components/NavbarTwo";
+import PricingAreaTwo from "../components/PricingAreaTwo";
+import ServiceAreaTwo from "../components/ServiceAreaTwo";
+import TestimonialOne from "../components/TestimonialOne";
+import WorkProcessTwo from "../components/WorkProcessTwo";
+// import FooterTwo from "../components/FooterTwo";
+import FooterOne from "../components/FooterOne";
+
+
+const HomeTwo = () => {
+  return (
+    <>
+      {/* Navigation Bar Two*/}
+      <NavbarTwo />
+
+      {/* Banner Two */}
+      <BannerTwo />
+
+      {/* About Area Two */}
+      <AboutAreaTwo />
+
+      {/* About Area Two */}
+      <ServiceAreaTwo />
+
+      {/* Pricing Area Two */}
+      <PricingAreaTwo />
+
+      {/* Counter Area Two */}
+      <CounterAreaTwo />
+
+      {/* Contact Area Two */}
+      <ContactAreaTwo />
+
+      {/* Work Process Two */}
+      <WorkProcessTwo />
+
+      {/* Testimonial One */}
+      <TestimonialOne />
+
+      {/* Blog Area Two */}
+      <BlogAreaTwo />
+
+      {/* Footer Two */}
+      {/* <FooterTwo /> */}
+      <FooterOne />
+
+    </>
+  );
+};
+
+export default HomeTwo;

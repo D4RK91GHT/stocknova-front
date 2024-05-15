50d6b73b4f1a0ee66b87daaf5e4dedf163a71c55
D4RK91GHT <rahulmajumdar400@gmail.com>
2024-05-15 23:03:01 +0530

    Stocks Component updated

--- src/pages/Stocks.js
+++ src/pages/Stocks.js
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import DashboardNavbar from "../components/DashboardNavbar";
 import Sidebar from "../components/Sidebar";
 
-import { Link } from "react-router-dom";
-import { Button, Card, Row, Col, Nav, ListGroup } from "react-bootstrap";
+import { Card, Row, Col, Nav, ListGroup } from "react-bootstrap";
 import ReactApexChart from "react-apexcharts";
-import { dp1, dp2, dp3 } from "../data/DashboardData";
+import { dp3 } from "../data/DashboardData";
+
 import "cryptofont/css/cryptofont.css";
 
 export default function Stocks() {
@@ -76,123 +76,6 @@ export default function Stocks() {
     };
   };
 
-  const seriesOne = [{
-    name: 'series1',
-    data: dp3
-  }, {
-    name: 'series2',
-    data: dp3
-  }, {
-    name: 'series3',
-    data: dp3
-  }];
-
-  const optionOne = {
-    chart: {
-      parentHeightOffset: 0,
-      stacked: true,
-      toolbar: { show: false }
-    },
-    colors: ['#0dcaf0', '#85b6fe', '#506fd9'],
-    grid: {
-      borderColor: 'rgba(72, 94, 144, .1)',
-      padding: {
-        top: -20
-      }
-    },
-    dataLabels: { enabled: false },
-    stroke: {
-      curve: 'straight',
-      width: 1.5
-    },
-    xaxis: {
-      type: 'numeric',
-      tickAmount: 6,
-      axisBorder: { show: false },
-      labels: {
-        style: {
-          colors: '#6e7985',
-          fontSize: '11px'
-        }
-      }
-    },
-    yaxis: {
-      min: 0,
-      max: 140,
-      tickAmount: 6,
-      labels: {
-        style: {
-          colors: '#6e7985',
-          fontSize: '11px'
-        }
-      }
-    },
-    fill: {
-      type: 'gradient',
-      gradient: {
-        opacityFrom: 0.5,
-        opacityTo: 0,
-      }
-    },
-    legend: { show: false },
-    tooltip: { enabled: false }
-  }
-
-
-  // Portfolio
-  const seriesTwo = [{
-    name: 'series1',
-    data: dp2
-  }, {
-    name: 'series2',
-    data: dp1
-  }];
-
-  const optionTwo = {
-    chart: {
-      parentHeightOffset: 0,
-      toolbar: { show: false },
-      sparkline: { enabled: true }
-    },
-    colors: ['#85b6ff', '#506fd9'],
-    grid: {
-      borderColor: 'rgba(72, 94, 144, .1)',
-      padding: {
-        top: -20,
-        left: 0
-      },
-      yaxis: { lines: { show: false } }
-    },
-    dataLabels: { enabled: false },
-    stroke: {
-      curve: 'straight',
-      width: 1.5
-    },
-    xaxis: {
-      type: 'numeric',
-      min: 5,
-      max: 90,
-      tickAmount: 6,
-      axisBorder: { show: false },
-      labels: {
-        style: {
-          colors: '#6e7985',
-          fontSize: '11px'
-        }
-      }
-    },
-    yaxis: { max: 180 },
-    fill: {
-      type: 'gradient',
-      gradient: {
-        opacityFrom: 0.5,
-        opacityTo: 0,
-      }
-    },
-    legend: { show: false },
-    tooltip: { enabled: false }
-  }
-
   const currentSkin = (localStorage.getItem('skin-mode')) ? 'dark' : '';
   const [skin, setSkin] = useState(currentSkin);
 
@@ -228,6 +111,30 @@ export default function Stocks() {
       <div className="main main-app p-3 p-lg-4">
         <Row className="g-3">
           <Col sm="6" xl="3">
+            <Card className="card-one">
+              <Card.Body className="overflow-hidden px-0">
+                <div className="crypto-item">
+                  <div className="avatar bg-orange"><i className="cf cf-btc"></i></div>
+                  <div className="crypto-body">
+                    <label className="card-label">Bitcoin <span>(BTC)</span></label>
+                    <h6 className="card-value">$3,972.87</h6>
+                  </div>
+                </div>
+                <ReactApexChart
+                  series={[{
+                    data: dp3
+                  }]}
+                  options={setOption('#fd7e14', 5, 55)}
+                  type="area"
+                  height={100}
+                  className="apex-chart-ten"
+                />
+              </Card.Body>
+              <Card.Footer className="card-footer-crypto pt-0">
+                <div><strong>12</strong> USD Markets</div>
+                <div><strong>80</strong> BTC Markets</div>
+              </Card.Footer>
+            </Card>
           </Col>
           <Col sm="6" xl="3">
             <Card className="card-one">
@@ -305,163 +212,6 @@ export default function Stocks() {
                 <div><strong>16</strong> USD Markets</div>
                 <div><strong>31</strong> DASH Markets</div>
               </Card.Footer>
-            </Card>
-          </Col>
-          <Col xl="8">
-            <Card className="card-one">
-              <Card.Header>
-                <Card.Title as="h6">Cryptocurrency Watchlist</Card.Title>
-                <Nav className="nav-icon nav-icon-sm ms-auto">
-                  <Nav.Link href=""><i className="ri-refresh-line"></i></Nav.Link>
-                  <Nav.Link href=""><i className="ri-more-2-fill"></i></Nav.Link>
-                </Nav>
-              </Card.Header>
-              <Card.Body>
-                <ReactApexChart series={seriesOne} options={optionOne} type="area" height={290} className="apex-chart-nine mb-4" />
-
-                <Row className="row-cols-auto gy-3 gx-5">
-                  {[
-                    {
-                      "label": "Market Cap",
-                      "value": "$14.5B"
-                    }, {
-                      "label": "Volume (24h)",
-                      "value": "$6.8B"
-                    }, {
-                      "label": "Change",
-                      "value": "$7.98"
-                    }, {
-                      "label": "Circulating Supply",
-                      "value": "$18.59M"
-                    }, {
-                      "label": "All Time High",
-                      "value": "$16.4K"
-                    }
-                  ].map((item, index) => (
-                    <Col key={index}>
-                      <label className="fs-xs mb-1 text-secondary">{item.label}</label>
-                      <h4 className="ff-numerals text-dark mb-0">{item.value}</h4>
-                    </Col>
-                  ))}
-                </Row>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col sm="6" xl="4">
-            <Card className="card-one">
-              <Card.Header>
-                <Card.Title as="h6">Bitcoin Wallet</Card.Title>
-                <Nav className="nav-icon nav-icon-sm ms-auto">
-                  <Nav.Link href=""><i className="ri-refresh-line"></i></Nav.Link>
-                  <Nav.Link href=""><i className="ri-more-2-fill"></i></Nav.Link>
-                </Nav>
-              </Card.Header>
-              <Card.Body>
-                <Card.Title as="label" className="fw-medium fs-sm mb-1">Available Balance</Card.Title>
-                <h3 className="card-value mb-1">26.00453100 <span className="text-secondary">BTC</span></h3>
-                <div className="d-flex gap-2 ff-numerals fs-sm">
-                  <div><span className="text-primary">USD</span> $103,342.50</div>
-                  <div><span className="text-success">EUR</span> $91,105.00</div>
-                </div>
-
-                <div className="d-flex gap-2 mt-4 mb-4">
-                  <Button variant="white" as="a" href="" className="flex-fill">Send</Button>
-                  <Button variant="primary" as="a" href="" className="flex-fill">Receive</Button>
-                </div>
-
-                <div className="d-flex align-items-center justify-content-between fs-xs mb-1">
-                  <label className="mg-b-0 fw-medium text-secondary">Recent Transactions</label>
-                  <Link href="">Show All</Link>
-                </div>
-
-                <ListGroup variant="flush">
-                  {[
-                    {
-                      "label": "Received Bitcoin",
-                      "value": "+0.00003998 BTC"
-                    }, {
-                      "label": "Sent Bitcoin",
-                      "value": "-0.01570525 BTC"
-                    }, {
-                      "label": "Ethereum Conversion",
-                      "value": "+2.0157 ETH"
-                    }, {
-                      "label": "Bought Bitcoin",
-                      "value": "+0.000033420 BTC"
-                    }, {
-                      "label": "Sent Bitcoin",
-                      "value": "-0.01570525 BTC"
-                    }
-                  ].map((item, index) => (
-                    <ListGroup.Item key={index} className="px-0 d-flex justify-content-between">
-                      <span className="fw-medium">{item.label}</span>
-                      <span className="ff-numerals">{item.value}</span>
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col sm="6" xl="4">
-            <Card className="card-one">
-              <Card.Header>
-                <Card.Title as="h6">Portfolio</Card.Title>
-                <Nav className="nav-icon nav-icon-sm ms-auto">
-                  <Nav.Link href=""><i className="ri-refresh-line"></i></Nav.Link>
-                  <Nav.Link href=""><i className="ri-more-2-fill"></i></Nav.Link>
-                </Nav>
-              </Card.Header>
-              <Card.Body>
-                <div className="position-absolute p-1">
-                  <label className="fw-medium fs-sm mb-1">Total Balance</label>
-                  <h3 className="card-value"><span className="text-secondary">$</span>7,428.68</h3>
-                </div>
-
-                <ReactApexChart series={seriesTwo} options={optionTwo} type="area" height={195} className="apex-chart-ten mb-4" />
-
-                <ListGroup as="ul" className="list-group-one">
-                  {[
-                    {
-                      "icon": "btc",
-                      "name": "Bitcoin",
-                      "value": "0.7200",
-                      "dollar": "2,907.71"
-                    }, {
-                      "icon": "eth",
-                      "name": "Ethereum",
-                      "value": "15.0030",
-                      "dollar": "2,083.62"
-                    }, {
-                      "icon": "ltc",
-                      "name": "Litecoin",
-                      "value": "12.5021",
-                      "dollar": "748.88"
-                    }, {
-                      "icon": "dash",
-                      "name": "Dash",
-                      "value": "16.5019",
-                      "dollar": "1,521.97"
-                    }, {
-                      "icon": "btc",
-                      "name": "Bitcoin Cash",
-                      "value": "5.8120",
-                      "dollar": "916.67"
-                    }
-                  ].map((item, index) => (
-                    <ListGroup.Item key={index} as="li" className="px-0 d-flex align-items-center gap-2">
-                      <div className="avatar bg-gray-300 text-secondary"><i className={"cf cf-" + item.icon}></i></div>
-                      <div>
-                        <h6 className="mb-0">{item.name}</h6>
-                        <small>{item.icon.toUpperCase()}/USD</small>
-                      </div>
-                      <div className="ms-auto text-end">
-                        <h6 className="ff-numerals mb-0">{item.value} {item.icon.toUpperCase()}</h6>
-                        <small className="text-secondary">${item.dollar} USD</small>
-                      </div>
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              </Card.Body>
             </Card>
           </Col>
           <Col sm="6" xl="4">
@@ -556,144 +306,6 @@ export default function Stocks() {
                     </ListGroup.Item>
                   ))}
                 </ListGroup>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col xl="7">
-            <Card className="card-one">
-              <Card.Header>
-                <Card.Title as="h6">Cryptocurrency News</Card.Title>
-                <Nav className="nav-icon nav-icon-sm ms-auto">
-                  <Nav.Link href=""><i className="ri-refresh-line"></i></Nav.Link>
-                  <Nav.Link href=""><i className="ri-more-2-fill"></i></Nav.Link>
-                </Nav>
-              </Card.Header>
-              <Card.Body>
-                {[
-                  {
-                    "img": "assets/img/crypto1.jpg",
-                    "author": "Crypto Ninja",
-                    "title": "Dow Futures, Bitcoin Teeter as Markets Wait for FOMC Bounce",
-                    "text": "As the unwelcome bearish momentum returns to all top cryptocurrency markets, most of this"
-                  }, {
-                    "img": "assets/img/crypto2.jpg",
-                    "author": "Cable News Network",
-                    "title": "XRP Price Remains Bearish as XRP/BTC Drops Below 7,800 Satoshi",
-                    "text": "Liquidity has shifted away from the top gaining crypto assets, with only six of the week’s 30 top performing"
-                  }, {
-                    "img": "assets/img/crypto3.jpg",
-                    "author": "Bitcoin.com",
-                    "title": "Bitcoin Price to $4500 soon? BTC Price Analysis",
-                    "text": "Published on CoinnounceTechnical Indicators: Support Level: $3900 Resistance Levels: $4100, $4200 Bitcoin"
-                  }
-                ].map((item, index) => (
-                  <div className="news-item" key={index}>
-                    <div className="news-img">
-                      <img src={item.img} className="img-fluid" alt="" /></div>
-                    <div className="news-body">
-                      <label className="d-block mb-1 fs-sm text-primary">{item.author}</label>
-                      <h6 className="news-title fw-semibold">
-                        <Link to="" className="text-dark">{item.title}</Link>
-                      </h6>
-                      <p className="news-text mb-0">{item.text}...</p>
-                    </div>
-                  </div>
-                ))}
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col sm="12" xl="5">
-            <Card className="card-one">
-              <Card.Header>
-                <Card.Title as="h6">Transactions</Card.Title>
-                <Nav className="nav-icon nav-icon-sm ms-auto">
-                  <Nav.Link href=""><i className="ri-refresh-line"></i></Nav.Link>
-                  <Nav.Link href=""><i className="ri-more-2-fill"></i></Nav.Link>
-                </Nav>
-              </Card.Header>
-              <Card.Body>
-                {[
-                  {
-                    "date": "Today, October 20",
-                    "transactions": [
-                      {
-                        "color": "success",
-                        "icon": "ri-coin-line",
-                        "title": "Received Bitcoin",
-                        "text": "Received via PayPal",
-                        "value": "+0.01084 BTC",
-                        "usd": "+25.60 USD",
-                        "success": true
-                      }, {
-                        "color": "primary",
-                        "icon": "ri-arrow-right-up-line",
-                        "title": "Sent Bitcoin",
-                        "text": "To Bitcoin Address",
-                        "value": "-0.00029 BTC",
-                        "usd": "-11.48 USD",
-                        "success": false
-                      }, {
-                        "color": "info",
-                        "icon": "ri-shopping-basket-line",
-                        "title": "Purchased Ethereum",
-                        "text": "Purchased using PayPal",
-                        "value": "+0.02115 ETH",
-                        "usd": "+21.50 USD",
-                        "success": true
-                      }, {
-                        "color": "info",
-                        "icon": "ri-shopping-basket-line",
-                        "title": "Purchased Bitcoin",
-                        "text": "Purchased using Debit Card",
-                        "value": "+0.0005 BTC",
-                        "usd": "+18.30 USD",
-                        "success": true
-                      }, {
-                        "color": "primary",
-                        "icon": "ri-arrow-right-up-line",
-                        "title": "Sent Bitcoin",
-                        "text": "To Bitcoin Address",
-                        "value": "-0.00040 BTC",
-                        "usd": "-18.50 USD",
-                        "success": false
-                      }
-                    ]
-                  }, {
-                    "date": "Yesterday, October 19",
-                    "transactions": [
-                      {
-                        "color": "success",
-                        "icon": "ri-coin-line",
-                        "title": "Received Bitcoin",
-                        "text": "Received via PayPal",
-                        "value": "+0.01084 BTC",
-                        "usd": "+25.60 USD",
-                        "success": true
-                      }
-                    ]
-                  }
-                ].map((item, index) => (
-                  <React.Fragment key={index}>
-                    <div className={"divider divider-start fw-medium" + ((index === 0) ? " mt-0" : "")}>
-                      <span>{item.date}</span>
-                    </div>
-                    <ListGroup as="ul" className="list-group-one">
-                      {item.transactions.map((trans, ind) => (
-                        <ListGroup.Item key={ind} as="li" className="px-0 d-flex align-items-center gap-2">
-                          <div className={`avatar border border-2 border-${trans.color} text-${trans.color}`}><i className={trans.icon}></i></div>
-                          <div>
-                            <h6 className="mb-0">{trans.title}</h6>
-                            <small>{trans.text}</small>
-                          </div>
-                          <div className="ms-auto text-end">
-                            <h6 className="ff-numerals mb-0">{trans.value}</h6>
-                            <small className={"text-" + (trans.success ? "success" : "danger")}>{trans.usd}</small>
-                          </div>
-                        </ListGroup.Item>
-                      ))}
-                    </ListGroup>
-                  </React.Fragment>
-                ))}
               </Card.Body>
             </Card>
           </Col>

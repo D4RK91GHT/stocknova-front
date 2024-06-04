1c6db4d5eeb27ea454570bc936a2abc3ae393bb0
D4RK91GHT <rahulmajumdar400@gmail.com>
2024-06-04 21:09:04 +0530

    Registration Completed

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,62 +1,3 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-
-// const LoginForm = () => {
-//   return (
-//     <>
-//       {/* ========================= contact Area One start =========================*/}
-//       <div className="login-area">
-//         <div className="container">
-//           <div className="login-section">
-//             <div className="row justify-content-center">
-//               <div className="col-lg-6">
-//                 <div className="sub-login-section mb-0">
-//                   <h6 className="sub-title">WELCOME BACK</h6>
-//                   <h2 className="title">
-//                     Bringing Your <span>Vision</span> To Life
-//                   </h2>
-//                   <form className="mt-4">
-//                     <div className="row">
-//                       <div className="col-lg-12">
-//                         <div className="single-input-inner style-border">
-//                           <input type="text" placeholder="Your Email" />
-//                         </div>
-//                       </div>
-
-//                       <div className="col-lg-12">
-//                         <div className="single-input-inner style-border">
-//                           <input type="password" placeholder="Password" />
-//                         </div>
-//                       </div>
-
-//                       <div className="col-12 mb-3">
-//                         <button
-//                           className="btn btn-black mt-0 w-100 border-radius-5"
-//                           href="#"
-//                         >
-//                           Login
-//                         </button>
-//                       </div>
-//                       <div className="text-center">
-//                         <Link className="mt-5" to="/register">
-//                           New Here ? Register Now
-//                         </Link>
-//                       </div>
-//                     </div>
-//                   </form>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       {/*========================= contact-inner One end =========================*/}
-//     </>
-//   );
-// };
-
-// export default LoginForm;
-
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -104,11 +45,9 @@ const LoginForm = () => {
           <div className="login-section">
             <div className="row justify-content-center">
               <div className="col-lg-6">
-                <div className="sub-login-section mb-0">
-                  <h6 className="sub-title">WELCOME BACK</h6>
-                  <h2 className="title">
-                    Bringing Your <span>Vision</span> To Life
-                  </h2>
+                <div className="sub-login-section mb-0 pt-4">
+                <img className="logo mb-2" src="assets/img/stocknova.png" alt="img" />
+
                   {error && <p className="text-danger">{error}</p>}
                   <form className="mt-4" onSubmit={handleLogin}>
                     <div className="row">
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,83 +1,6 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-
-// const RegisterForm = () => {
-//   return (
-//     <>
-//       {/* ========================= contact Area One start =========================*/}
-//       <div className="login-area">
-//         <div className="container">
-//           <div className="login-section">
-//             <div className="row justify-content-center">
-//               <div className="col-lg-6">
-//                 <div className="sub-login-section mb-0">
-//                   <h6 className="sub-title">GLAD TO SEE YOU</h6>
-//                   <h2 className="title">
-//                     Bringing Your <span>Vision</span> To Life
-//                   </h2>
-//                   <form className="mt-4">
-//                     <div className="row">
-                      
-//                       <div className="col-lg-6">
-//                         <div className="single-input-inner style-border">
-//                           <input type="text" placeholder="Fisrt Name" />
-//                         </div>
-//                       </div>
-
-//                       <div className="col-lg-6">
-//                         <div className="single-input-inner style-border">
-//                           <input type="text" placeholder="Last Name" />
-//                         </div>
-//                       </div>
-
-//                       <div className="col-lg-12">
-//                         <div className="single-input-inner style-border">
-//                           <input type="mail" placeholder="Your Email" />
-//                         </div>
-//                       </div>
-
-//                       <div className="col-lg-12">
-//                         <div className="single-input-inner style-border">
-//                           <input type="password" placeholder="Password" />
-//                         </div>
-//                       </div>
-
-//                       <div className="col-12 mb-3">
-//                         <button
-//                           className="sn-btn mt-0 w-100 border-radius-5"
-//                           href="#"
-//                         >
-//                           Register
-//                         </button>
-//                       </div>
-//                       <div className="text-center">
-//                         <Link className="mt-5" to="/login">
-//                           Already Registered? Login Here
-//                         </Link>
-//                       </div>
-//                       <div className="text-center py-2">
-//                         <Link className="text-black" to="/">
-//                           Go Back
-//                         </Link>
-//                       </div>
-//                     </div>
-//                   </form>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       {/*========================= contact-inner One end =========================*/}
-//     </>
-//   );
-// };
-
-// export default RegisterForm;
-
-
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Alert } from "react-bootstrap";
 import axios from "axios";
 
 const RegisterForm = () => {
@@ -86,63 +9,46 @@ const RegisterForm = () => {
   const [LName, setLName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate();
+  const [cpassword, setCPassword] = useState("");
 
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const formData = new FormData();
-      formData.append("email", FName);
-      formData.append("email", LName);
+      formData.append("fname", FName);
+      formData.append("lname", LName);
       formData.append("email", email);
       formData.append("password", password);
+      console.log(FName, LName, email, password);
 
       const response = await axios.post(
         "http://127.0.0.1:8000/register/",
         formData
       );
       if (response.status === 200) {
-        console.log(response);
         if (response.data.status === true) {
-          // navigate("/dashboard");
+          if (response.data.status) {
+            navigate("/dashboard");
+            // console.log(response.data);
+          }
         } else {
-          setError(response.data.message);
+          console.log(response.data.error);
+          setError(response.data.error);
         }
       } else {
         if (response.data.status === false) {
           setError(response.data.message);
+        } else {
+          console.log(response.data);
+          setError(response.data.message);
         }
       }
     } catch (err) {
-      setError("Error:", JSON.stringify(error));
-      console.error("Error:", JSON.stringify(error));
+      setError("Error:", JSON.stringify(err));
+      console.error("Error:", JSON.stringify(err));
     }
-    // const { email, password } = formData;
-
-    // let data = new FormData();
-    // data.append('email', email);
-    // data.append('password', password);
-
-    // let config = {
-    //   method: 'post',
-    //   maxBodyLength: Infinity,
-    //   url: 'http://127.0.0.1:8000/register/',
-    //   headers: { 
-    //     ...data.getHeaders()
-    //   },
-    //   data : data
-    // };
-
-    // axios.request(config)
-    //   .then((response) => {
-    //     console.log(JSON.stringify(response.data));
-    //     // You can handle success here, e.g., redirect to another page or show a success message
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //     // You can handle error here, e.g., show an error message
-    //   });
   };
 
   return (
@@ -153,14 +59,12 @@ const RegisterForm = () => {
           <div className="login-section">
             <div className="row justify-content-center">
               <div className="col-lg-6">
-                <div className="sub-login-section mb-0">
-                  <h6 className="sub-title">GLAD TO SEE YOU</h6>
-                  <h2 className="title">
-                    Bringing Your <span>Vision</span> To Life
-                  </h2>
+                <div className="sub-login-section mb-0 pt-4">
+                  <img className="logo mb-2" src="assets/img/stocknova.png" alt="img" />
+
+                  {error ? <Alert variant="warning">{error}</Alert> : ""}
                   <form className="mt-4" onSubmit={handleSubmit}>
                     <div className="row">
-                      
                       <div className="col-lg-6">
                         <div className="single-input-inner style-border">
                           <input
@@ -205,6 +109,18 @@ const RegisterForm = () => {
                             name="password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
+                          />
+                        </div>
+                      </div>
+
+                      <div className="col-lg-12">
+                        <div className="single-input-inner style-border">
+                          <input
+                            type="password"
+                            placeholder="Confirm Password"
+                            name="cpassword"
+                            value={cpassword}
+                            onChange={(e) => setCPassword(e.target.value)}
                           />
                         </div>
                       </div>

7494853e35cc5e938d4a58ebae00a5d39eb5c0ab
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2024-06-05 11:57:03 +0530

    Footer Updated

--- src/components/FooterOne.js
+++ src/components/FooterOne.js
@@ -16,42 +16,22 @@ const FooterOne = () => {
     <>
       {/* ================== Footer One Start ==================*/}
       <footer className='footer-area bg-black bg-cover'>
-        {/* <div className='footer-subscribe'>
-          <div className='container'>
-            <div
-              className='footer-subscribe-inner bg-cover'
-              style={{ backgroundImage: 'url("./assets/img/bg/6.png")' }}
-            >
-              <div className='row'>
-                <div className='col-lg-6'>
-                  <h2 className='mb-lg-0 mb-3'>Subscribe To Our Newsletter</h2>
-                </div>
-                <div className='col-lg-6 align-self-center text-lg-end'>
-                  <input type='text' placeholder='Your e-mail address' />
-                  <a className='btn btn-black border-radius-0' href='#'>
-                    Submit now
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
         <div className='container'>
           <div className='row'>
-            <div className='col-lg-3 col-md-6'>
+            <div className='col-lg-4 col-md-6'>
               <div className='widget widget_about'>
                 <div className='thumb'>
                   <img src='assets/img/stocknova.png' alt='img' />
                 </div>
                 <div className='details'>
                   <p>
-                    Melbourne is simply is dumiomy is text Lorem Ipsum is simply
+                    Where Data Meets Destiny
                   </p>
                   <p className='mt-3'>
-                    <FaPhoneAlt /> (+888) 123 456 765
+                    <FaPhoneAlt /> (+91) 7699753019
                   </p>
                   <p className='mt-2'>
-                    <FaEnvelope /> (+888) 123 456 765
+                    <FaEnvelope /> (+91) 7318984157
                   </p>
                   <ul className='social-media'>
                     <li>
@@ -78,120 +58,63 @@ const FooterOne = () => {
                 </div>
               </div>
             </div>
-            <div className='col-lg-3 col-md-6'>
+            <div className='col-lg-4 col-md-6'>
               <div className='widget widget_nav_menu'>
-                <h4 className='widget-title'>Our Service</h4>
+                <h4 className='widget-title'>Links</h4>
                 <ul>
                   <li>
-                    <Link to='/service'>
-                      <FaArrowRight /> Ui Design
+                    <Link to='/'>
+                      <FaArrowRight /> Stock List
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to='/'>
+                      <FaArrowRight /> NSE
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to='/'>
+                      <FaArrowRight /> BSE{" "}
                     </Link>
                   </li>
                   <li>
                     <Link to='/service'>
-                      <FaArrowRight /> Web design
+                      <FaArrowRight /> SEBI
                     </Link>
                   </li>
                   <li>
                     <Link to='/service'>
-                      <FaArrowRight /> Digital marketing{" "}
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/service'>
-                      <FaArrowRight /> Video Editing
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/service'>
-                      <FaArrowRight /> Pc Repairs
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/service'>
-                      <FaArrowRight /> Web Development
+                      <FaArrowRight /> Meet Advisor
                     </Link>
                   </li>
                 </ul>
               </div>
             </div>
-            <div className='col-lg-3 col-md-6'>
+            <div className='col-lg-4 col-md-6'>
               <div className='widget widget_nav_menu'>
-                <h4 className='widget-title'>Pay links</h4>
+                <h4 className='widget-title'>Important</h4>
                 <ul>
                   <li>
-                    <Link to='/service'>
-                      <FaArrowRight /> Credit industrys
+                    <Link to='/'>
+                      <FaArrowRight /> Inversor Comunity
                     </Link>
                   </li>
                   <li>
                     <Link to='/service'>
-                      <FaArrowRight /> Reasearch sector
+                      <FaArrowRight /> Finance Creator
                     </Link>
                   </li>
                   <li>
-                    <Link to='/service'>
-                      <FaArrowRight /> Finance Sector{" "}
+                    <Link to='/'>
+                      <FaArrowRight /> About Us
                     </Link>
                   </li>
                   <li>
-                    <Link to='/service'>
-                      <FaArrowRight /> Credit industrys
+                    <Link to='/'>
+                      <FaArrowRight /> Contact Us{" "}
                     </Link>
                   </li>
-                  <li>
-                    <Link to='/service'>
-                      <FaArrowRight /> Reasearch sector
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to='/service'>
-                      <FaArrowRight /> Finance Sector
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className='col-lg-3 col-md-6'>
-              <div className='widget widget-recent-post'>
-                <h4 className='widget-title'>Contact us</h4>
-                <ul>
-                  <li>
-                    <div className='media'>
-                      <div className='media-left'>
-                        <img src='assets/img/widget/1.png' alt='blog' />
-                      </div>
-                      <div className='media-body align-self-center'>
-                        <div className='post-info mb-2'>
-                          <FaCalendarAlt />
-                          <span>january 11, 2023</span>
-                        </div>
-                        <h6 className='title mb-0'>
-                          <Link to='/blog-details'>
-                            Social Media For Promote Business.
-                          </Link>
-                        </h6>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className='media'>
-                      <div className='media-left'>
-                        <img src='assets/img/widget/2.png' alt='blog' />
-                      </div>
-                      <div className='media-body align-self-center'>
-                        <div className='post-info mb-2'>
-                          <FaCalendarAlt />
-                          <span>january 11, 2023</span>
-                        </div>
-                        <h6 className='title mb-0'>
-                          <Link to='/blog-details'>
-                            Marketing For Base market watch
-                          </Link>
-                        </h6>
-                      </div>
-                    </div>
-                  </li>
+                  
                 </ul>
               </div>
             </div>
